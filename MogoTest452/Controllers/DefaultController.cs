2454857abc0aa23391ab8f5e0a8272ff1ce997fd
Yunus KIRAN <yunus.kiran@besiktas.li>
2019-06-19 13:41:49 +0300

    init

---
+++ MogoTest452/Controllers/DefaultController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using MongoDB.Driver;
+
+namespace MogoTest452.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        private const string connectionString = "mongodb://nems:123456@10.201.60.204";
+        private IMongoClient _mongoClient;
+        public DefaultController()
+        {
+        }
+
+        [Route("api/default/dowork")]
+        [HttpGet]
+        public IHttpActionResult DoWork()
+        {
+            try
+            {
+                _mongoClient = new MongoClient(connectionString);
+                return Ok("ok");
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
+    }
+}
---
+++ MogoTest452/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MogoTest452.WebApiApplication" Language="C#" %>
